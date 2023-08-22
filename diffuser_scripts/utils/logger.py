756d9107324e0acb059d4ec5d9b47aa19fc0281c
mephisto28 <462464479@qq.com>
2023-08-22 14:25:33 +0800

    refine logger, using loguru

--- diffuser_scripts/utils/logger.py
+++ diffuser_scripts/utils/logger.py
@@ -1,26 +1,23 @@
 import os
+import sys
 import time
 import json
-import logging
+from loguru import logger
+
+import cv2
 from PIL import Image
 
-
-logging.basicConfig(
-    format='[%(asctime)s %(name)-8s'
-           '%(levelname)s %(process)d '
-           '%(filename)s:%(lineno)-5d]'
-           ' %(message)s',
-    level=logging.INFO
-)
-
-logger = logging#.getLogger()
+logger.add(sys.stderr, format="{time} {level} {message}", level="INFO")
 
 
 def dump_image_to_dir(image: Image.Image, output_dir: str, name=''):
     timestamp = str(int(time.time()))
     os.makedirs(output_dir, exist_ok=True)
     output_path = os.path.join(output_dir, timestamp + '_%s.png' % name)
-    image.save(output_path)
+    if isinstance(image, Image.Image):
+        image.save(output_path)
+    else:
+        cv2.imwrite(output_path, image)
 
 
 def dump_request_to_file(task, output_dir: str, name='task'):
