6965a74216568cf13650ddf7aa64299c7e71c09d
mephisto28 <462464479@qq.com>
2023-08-28 21:44:56 +0800

    add test id network

--- test/test_app.py
+++ test/test_app.py
@@ -13,12 +13,14 @@ from diffuser_scripts.utils.decode import encode_image_b64, decode_image_b64
 @fire.Fire
 def test_post_app(port='1234', random_seed=-1):
     image = np.array(Image.open("images/lc_silence.png"))
+    wc = np.array(Image.open('/mnt/lg104/character_dataset/preprocessed_v0/万茜/34.png'))
+
     print(image.shape)
     obj = {
         'prompt':[
             "1woman and 1man, drink",
             "baijingting",
-            "ouyangnana"
+            "1woman"
         ],
         "negative_prompt": [
             'paintings, sketches, (worst quality:2), (low quality:2), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, age spot, glans, lowres,bad anatomy,bad hands, text, error, missing fingers,extra digit, fewer digits, cropped, worstquality, low quality, normal quality,jpegartifacts,signature, watermark, username,blurry,bad feet,cropped,poorly drawn hands,poorly drawn face,mutation,deformed,worst quality,low quality,normal quality,jpeg artifacts,watermark,extra fingers,fewer digits,extra limbs,extra arms,extra legs,malformed limbs,fused fingers,too many fingers,long neck,cross-eyed,mutated hands,polar lowres,bad body,bad proportions,gross proportions,text,error,missing fingers,missing arms,missing legs,extra digit,(nsfw:1.5), (sexy)',
@@ -35,9 +37,11 @@ def test_post_app(port='1234', random_seed=-1):
                 "exp0814_baijingting_single-000005": 0.8
             },
             {
-                "exp0814_ouyangnana_single-000005": 0.8
+                "035_mix-000008": 1
             }
         ],
+        "id_reference_img": [None, None, encode_image_b64(wc)],
+        "add_id_feature": [False, False, True],
         "condition_img_str": encode_image_b64(image),
         "control_guidance_scale": 1.0,
         "control_guidance_end": 0.5,
@@ -49,7 +53,7 @@ def test_post_app(port='1234', random_seed=-1):
         "latent_mask_weight_decay": 0.03,
         "control_mode": "prompt",
         "control_scale_decay_ratio": 0.825,
-        "debug_steps": [3, 6, 9, 12, 15, 18, 21, 24, 27]
+        "debug_steps": []
     }
     start = time.time()
     response = requests.post("http://192.168.110.102:%s/get_latent_couple" % port, data=json.dumps(obj))
